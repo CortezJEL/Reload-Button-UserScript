686d6f02ac25ff8d460d8b49feca86f39f006ed3
Nanashi <johnnyjel1234567890@gmail.com>
2023-12-01 22:50:44 -0600

    Create Reload_Button.js

---
+++ Reload_Button.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name            Reload-Button-UserScript
+// @description     Adds a simple reload button
+// @license         AGPL-V3
+// @version         1.0
+// @author          John Litzow
+// @match           *://*.*/*
+// @include         *://*.*/*
+// @grant           GM_registerMenuCommand
+// ==/UserScript==
+
+// Reload the current page
+function reload() {
+  try {
+    window.location.reload(false);
+  } catch (e) {
+    console.error("Error reloading the page:", e);
+  }
+}
+
+// Fully reload the current page
+function fullReload() {
+  try {
+    window.location.reload(true);
+  } catch (e) {
+    console.error("Error full reloading the page:", e);
+  }
+}
+
+// Register the menu commands
+GM_registerMenuCommand("Fast reload.", reload, "r");
+GM_registerMenuCommand("Full reload.", fullReload, "R");
